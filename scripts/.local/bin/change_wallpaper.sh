f385f888143bd773a07f193542c66a9c58d2e36f
bitnerry <170930999+bitnerry@users.noreply.github.com>
2026-05-01 13:36:58 +0530

    Make wallpaper script portable (use HOME paths)

--- scripts/.local/bin/change_wallpaper.sh
+++ scripts/.local/bin/change_wallpaper.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-WALLPAPER_DIR="/home/sidharth/walli/onlyarch/"
-STATE_FILE="/home/sidharth/.cache/wallpaper_index"
+WALLPAPER_DIR="$HOME/walli/onlyarch/"
+STATE_FILE="$HOME/.cache/wallpaper_index"
 
 # Get sorted list of wallpapers
 mapfile -t wallpapers < <(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.bmp" -o -iname "*.gif" \) | sort)
 
 if [ ${#wallpapers[@]} -eq 0 ]; then
-    echo "No wallpapers found in $WALLPAPER_DIR"
-    exit 1
+  echo "No wallpapers found in $WALLPAPER_DIR"
+  exit 1
 fi
 
 # Read last index, default to -1
 index=$(cat "$STATE_FILE" 2>/dev/null || echo -1)
 
 # Move to next wallpaper, wrap around at the end
-index=$(( (index + 1) % ${#wallpapers[@]} ))
+index=$(((index + 1) % ${#wallpapers[@]}))
 
 # Save new index
-echo "$index" > "$STATE_FILE"
+echo "$index" >"$STATE_FILE"
 
 wallpaper="${wallpapers[$index]}"
 echo "Applying: $wallpaper ($((index + 1))/${#wallpapers[@]})"
